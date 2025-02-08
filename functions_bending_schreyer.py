597f1908bd66040c7b9cc25e8568b2c4173a6b42
bensch <crafte23@gmail.com>
2025-02-08 15:08:11 -0500

    Secant method shooting, is ~2x as fast

--- functions_bending_schreyer.py
+++ functions_bending_schreyer.py
@@ -98,7 +98,45 @@ def binary_search_bending(L, R, grid, df_ds, tol, theta0,search_param):
         return binary_search_bending((L+R)/mpmathify(2), R, grid, df_ds, tol, theta0,search_param)#return binary_search_bending((L+R)/mpmathify(2), R,EXP,I_z,key, tol)
     return (L + R)/mpmathify(2)
 
+def secant_method_bending(L, R, grid, df_ds, tol, theta0, search_param):
+    s0 = grid[0]
+    ic = [0, 0, 0]
+    
+    # Initial guesses for secant method
+    ic[search_param] = L
+    S1, F1, Es1 = mp_RKF45_fixed(mp.matrix(ic), s0, grid[len(grid) - 1], df_ds, tol, grid[1] - grid[0])
+    ic[search_param] = R
+    S2, F2, Es2 = mp_RKF45_fixed(mp.matrix(ic), s0, grid[len(grid) - 1], df_ds, tol, grid[1] - grid[0])
+    
+    # First value at R
+    F1_value = F1[-1][1]
+    F2_value = F2[-1][1]
+    
+    print(F2_value)
 
+    # Continue iterating using the secant method
+    iteration = 0
+    while mp.fabs(F2_value - theta0) / theta0 >= tol and mp.fabs(L - R) >= tol * (L + R):
+        # Update guess using the secant method formula
+        if F2_value - F1_value == 0:  # Prevent division by zero
+            return (L + R) / mp.mpf(2)  # Return midpoint if derivative is zero
+        new_L = R - (F2_value - theta0) * (R - L) / (F2_value - F1_value)
+
+        # Update values based on the new guess
+        ic[search_param] = new_L
+        S_new, F_new, Es_new = mp_RKF45_fixed(mp.matrix(ic), s0, grid[len(grid) - 1], df_ds, tol, grid[1] - grid[0])
+        F_new_value = F_new[-1][1]
+
+        # Update variables for the next iteration
+        L = R
+        F1_value = F2_value
+        R = new_L
+        F2_value = F_new_value
+        
+        iteration += 1
+        print(F2_value)
+
+    return R
 
 #Bending calculation for a zero moment zero theta initial flexure with "shape" Isamples
 #grid: locations along the fiber the moment is sampled, Isamples: samples of the moment for bending along the fiber (same length as grid), order: not used, E: modulus of the material, Fsin: force coefficent for the sin term in bending, Fcos: boolean determining if a side force is present, theta0: intended bending angle, tol: tolerance for shooting
@@ -167,12 +205,12 @@ def bend_samples(grid, Isamples, order = 4, E = 1, Fsin = mpmathify(0), Fcos = T
     #Large angle we now need to binary search to find the initial condition by shooting
     #Sine only, which means shootign a small initial bending moment
     if Fs and (not Fcos):
-        f0[0] = binary_search_bending(f0[0]/mpmathify(32), f0[0]* mpmathify(32), grid, df_ds, tol, theta0, 0)
+        f0[0] = secant_method_bending(f0[0]/mpmathify(32), f0[0]* mpmathify(32), grid, df_ds, tol, theta0, 0)#binary_search_bending(f0[0]/mpmathify(32), f0[0]* mpmathify(32), grid, df_ds, tol, theta0, 0)
         S, F, Es = mp_RKF45_fixed(f0,s0,grid[len(grid) - 1],df_ds,tol,grid[1] - grid[0])
         return S, F, Es
     #Cosine only, shoots the F2 term which is the force coefficient on the cosine force
     if (not Fs) and Fcos:
-        f0[2] = binary_search_bending(f0[2]/mpmathify(32), f0[2]* mpmathify(32), grid, df_ds, tol, theta0, 2)
+        f0[2] = secant_method_bending(f0[2]/mpmathify(32), f0[2]* mpmathify(32), grid, df_ds, tol, theta0, 2)#binary_search_bending(f0[2]/mpmathify(32), f0[2]* mpmathify(32), grid, df_ds, tol, theta0, 2)
         S, F, Es = mp_RKF45_fixed(f0,s0,grid[len(grid) - 1],df_ds,tol,grid[1] - grid[0])
         return S, F, Es
     
